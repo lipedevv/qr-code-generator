0845afde2a81099fa7d213cf0d4883a76d73bb91
lipedevv <codecorebots@gmail.com>
2024-10-13 00:59:12 -0300

    aaaaaaaa

--- src/js/index.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-    $('#generate').click(function() {
-        var text = $('#text-input').val();
-        $('#qrcode').empty();
-        $('#qrcode').qrcode(text);
-    });
-});
